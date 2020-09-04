c94ef6a5e5651bc41e4471b290ac450608fdb559
tejaspandit99 <pandit.tejas2017@gmail.com>
2020-09-05 00:01:42 +0530

    Updates

---
+++ precache-manifest.0d0799897057c93263642862d9d9bd44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d965e72a33e9cdbb4c4cdd69232165e",
+    "url": "/Monsters-rolldex/index.html"
+  },
+  {
+    "revision": "41da45c6f5afa065eb6c",
+    "url": "/Monsters-rolldex/static/css/main.367c9f3c.chunk.css"
+  },
+  {
+    "revision": "85fa06eb2609d7d37821",
+    "url": "/Monsters-rolldex/static/js/2.1c60bc2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolldex/static/js/2.1c60bc2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41da45c6f5afa065eb6c",
+    "url": "/Monsters-rolldex/static/js/main.f2f4fb83.chunk.js"
+  },
+  {
+    "revision": "e388fe4a638dd7b56b00",
+    "url": "/Monsters-rolldex/static/js/runtime-main.bc1b9fdd.js"
+  }
+]);
